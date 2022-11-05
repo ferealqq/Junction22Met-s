81b7a0598f6d966fc2a51c1853813087f9e0c1ef
Aleksi Puttonen <aleksi.puttonen@gmail.com>
2022-11-05 10:01:20 +0200

    Mock tasks

--- backend/app/routers/tasks.py
+++ backend/app/routers/tasks.py
@@ -3,6 +3,7 @@ from typing import Optional, List
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.db.deps import get_db
+
 # from app.db.utils import save_model
 
 from app.models.task import Task, TaskOut
@@ -20,6 +21,7 @@ async def get_tasks(
 ):
     return db.query(Task).limit(limit).offset(skip).all()
 
+
 @router.get("/active", response_model=List[TaskOut])
 async def get_active_tasks(
     search: Optional[str] = None,
@@ -28,3 +30,27 @@ async def get_active_tasks(
     db: Session = Depends(get_db),
 ):
     return db.query(TaskActivity).limit(limit).offset(skip).all()
+
+
+@router.get("/mock_tasks")
+async def get_mock_tasks():
+    return [
+        {
+            "id": 1,
+            "title": "Task 1",
+            "desc": "Task 1 description",
+            "emission": 1.0,
+        },
+        {
+            "id": 2,
+            "title": "Task 2",
+            "desc": "Task 2 description",
+            "emission": 2.0,
+        },
+        {
+            "id": 3,
+            "title": "Task 3",
+            "desc": "Task 3 description",
+            "emission": 3.0,
+        },
+    ]
