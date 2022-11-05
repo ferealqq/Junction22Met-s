883854b859ff185b6f08dc2f382af90c9f349c33
Aleksi Puttonen <aleksi.puttonen@gmail.com>
2022-11-05 11:58:30 +0200

    Delete console log

--- metas/src/service/getTasks.ts
+++ metas/src/service/getTasks.ts
@@ -2,6 +2,5 @@ import axios from "axios";
 
 export const getTasks = async () => {
   const response = await axios.get("http://localhost:3004/tasks");
-  console.log(response.data);
   return response.data;
 };
