c10591fb225860b7565f229e0c242ffa5e5c03e1
ferealqq <pekka.mattinen@webso.fi>
2022-11-05 23:41:29 +0200

    reset database script created

---
+++ backend/resetdb.py
@@ -0,0 +1,16 @@
+import time
+import uuid
+from dotenv import load_dotenv
+from app.db.base import engine
+from tests.conftest import drop_everything
+
+load_dotenv()
+
+drop_everything(engine)
+
+import os
+os.system('poetry run alembic upgrade head')
+
+time.sleep(1)
+
+import seed
