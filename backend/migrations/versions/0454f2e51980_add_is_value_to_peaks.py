f2dec9f5f45c6b43c8abcee406c7e3d123d153c6
ferealqq <pekka.mattinen@webso.fi>
2022-11-05 17:37:08 +0200

    maybe fix

---
+++ backend/migrations/versions/0454f2e51980_add_is_value_to_peaks.py
@@ -0,0 +1,24 @@
+"""add is value to peaks
+
+Revision ID: 0454f2e51980
+Revises: c65a165d8fbf
+Create Date: 2022-11-05 17:31:53.034302
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0454f2e51980'
+down_revision = 'c65a165d8fbf'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
