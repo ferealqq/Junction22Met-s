188e320e2a36a9b973d10e2325f9cf4739ffa3a2
ferealqq <pekka.mattinen@webso.fi>
2022-11-05 16:20:12 +0200

    analytics seeder and create token improved

--- backend/create_token.py
+++ backend/create_token.py
@@ -1,9 +1,23 @@
 import uuid
 from dotenv import load_dotenv
+from app.db.deps import get_db
+from app.db.utils import save_model
 from app.dependencies.auth import JWTService
+import app.models.task_completion
+from app.models.user import User
 
 load_dotenv()
 
-data = {"id": str(uuid.uuid4())}
+db = next(get_db())
+
+name = input("Type username: ")
+
+user = db.query(User).filter(User.username == name).one_or_none()
+if user == None:
+  c = input("user not found create new? Enter for YES, press any other key to cancel")
+  if c == "" or c == None:
+    user = save_model(db,User(user))
+
+data = {"id": str(user.id)}
 
 print(JWTService().encode(data))
