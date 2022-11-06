19fc811fe62d2911e81bd6a4f444091a5cca4573
Aleksi Puttonen <aleksi.puttonen@gmail.com>
2022-11-06 02:49:50 +0200

    Get one user

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -17,8 +17,6 @@ router = APIRouter()
 @router.get("/{user_id}")
 async def get_user(user_id: str, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.id == user_id).one_or_none()
-    if user == None:
-        raise NOT_FOUND_EXCEPTION
     return user
 
 
