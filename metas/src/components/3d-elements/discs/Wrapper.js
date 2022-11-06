7c922ae5caeae475617fd9db977ec5dad0c9ef77
Aleksi Puttonen <aleksi.puttonen@gmail.com>
2022-11-06 02:16:14 +0200

    Get stats

--- metas/src/components/3d-elements/discs/Wrapper.js
+++ metas/src/components/3d-elements/discs/Wrapper.js
@@ -33,27 +33,4 @@ export const WrapperForDiscs = () => {
     return <CommunityFull rotationSpeed={0.1} />;
   }
   return <Large1Disc rotationSpeed={0.3} />;
-  //   const [zoom, setZoom] = useState(false);
-  //   const [focus, setFocus] = useState(true);
-  //   const vec = new THREE.Vector3();
-  //   useFrame((state) => {
-  //     const step = 0.05;
-
-  //     /*
-  //       Need to find some way to lerp the lookAt
-  //       */
-  //     // @ts-ignore
-  //     zoom ? vec.set(focus.x, focus.y, focus.z + 0.2) : vec.set(0, 0, 5);
-  //     //
-  //     state.camera.position.lerp(vec, step);
-  //     state.camera.lookAt(0, 0, 0);
-  //     // Update to new position/lookAt
-  //     state.camera.updateProjectionMatrix();
-  //   });
-
-  //   const zoomToView = (focusRef) => {
-  //     setZoom(!zoom);
-  //     setFocus(focusRef.current.position);
-  //   };
-  //   return <DiscSmall rotationSpeed={0.3} zoomToView={zoomToView} />;
 };
--- metas/src/data/api.ts
+++ metas/src/data/api.ts
@@ -1,15 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 const token = `eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjkzNThjNTFiLTdkMDgtNGIwMy1iYWZlLWMyMjY2N2I4Mzc4NiIsImV4cCI6MTY2ODI3NTE1MSwiaWF0IjoxNjY3NjcwMzUxfQ.Ckz64W4CrTNiMCeBjDn5cEXtwFlhIm_A65LWoJpn7yVOe_JIf0x_ycua0qvDFoghtQXIzllBAO9kWwAWbbugBgAimk3o97NFC2owaUxQGG8IXEeioFuOl5RnJM_hLbWHv5pmaEbWDO1Y-M4OjcsT5s8wj4LsAPbHKaGsVedrqQhbkeaelpkYZYl-KIm1wLEivCAE4npXsZvFEUpnPy65BA-UiIkMtxJjwcqZwU6S4pNd6jpS2Qq2smpX81xU-GuUl3f3eVxScm_8VaTj6cseErFrAsOtgYy-BiFy-EQNYT_j9-D_dFstQKa6OmFgJC92HySnq-VnJ0Co7mq155x6QA`;
 
 export const api = axios.create({
   baseURL: `https://metsabakkari.fly.dev/api/`,
   headers: {
-    "Authorization": `Bearer ${token}`
-  }
-})
-
+    Authorization: `Bearer ${token}`,
+  },
+});
 
 export const fetchAnalytics = () => {
-  return api.get("tasks/user/analytics").then(({data}) => data)
+  return api.get("tasks/emission/analytics").then(({ data }) => data);
 };
